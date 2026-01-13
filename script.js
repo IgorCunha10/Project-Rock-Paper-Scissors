df49753cba647eb8a59fa3f46bfd4784cd60644e
Igor Cunha <[200~git config --global user.email>
2026-01-13 18:44:16 -0300

    Created the function that will define the computer choice

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-function getComputerChoice() {
+function getComputerChoice(min, max) {
+const minCeiled = Math.ceil(min);
+const maxFloored = Math.floor(max);
+return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+    
+}
+
+const random = getComputerChoice(1, 3);
+let computerChoice;
+
+if (random === 1) {
+    computerChoice = "Rock";
+} else if (random === 2) {
+    computerChoice = "Paper"
+} else {
+    computerChoice = "Scissor"
 
 }
+
+console.log(computerChoice);
 
 function getHumanChoice(){
     
 }
+
+
+
+function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+  }
+
